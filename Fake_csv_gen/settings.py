435117a64db89f29d023e0f45b3d5f85092e3678
Error <shkum79@ukr.net>
2023-03-11 21:09:43 +0100

    update

--- Fake_csv_gen/settings.py
+++ Fake_csv_gen/settings.py
@@ -110,11 +110,11 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'csv_gen/static'),
+    os.path.join(BASE_DIR, 'csv_gen', 'static'),
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
-
+# STATIC_ROOT = os.path.join(BASE_DIR, 'csv_gen', "static")
+#
 MEDIA_ROOT = os.path.join(BASE_DIR, 'csv_gen', 'media')
 
 MEDIA_URL = '/media/'
